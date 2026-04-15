d3b684042bd36587e10d6f8db8e4d9cccc1570f1
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-04-15 18:25:44 -0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
